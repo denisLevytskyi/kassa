b2191c43af321b34a8c7e51d59a7108138004d95
Your Name <tesu@example.com>
2022-11-11 23:59:06 +0200

    ADD MIGRATION DB

--- Logic/Connection.php
+++ Logic/Connection.php
@@ -5,7 +5,7 @@ class Connection {
 	const host = 'localhost';
 	const user = 'root';
 	const password = '';
-	const bd = 'test';
+	const bd = 'product_meneger';
 
 	public static function get_connection () {
 		$connection = mysqli_connect(
@@ -13,6 +13,15 @@ class Connection {
 			self::user,
 			self::password,
 			self::bd
+		);
+		return $connection;
+	}
+
+	public static function get_first_connection () {
+		$connection = mysqli_connect(
+			self::host,
+			self::user,
+			self::password
 		);
 		return $connection;
 	}
--- _migration.php
+++ _migration.php
@@ -1,6 +1,11 @@
 <?php 
 include 'Logic/Autoload.php';
 
+$f_connection = Logic\Connection::get_first_connection();
+$bd = Logic\Connection::bd;
+$request0 = "CREATE DATABASE `$bd`";
+$rezult0 = mysqli_query($f_connection, $request0);
+
 $connection = Logic\Connection::get_connection();
 $request1 = "CREATE TABLE `users` (
 	`id` INT NOT NULL AUTO_INCREMENT,
@@ -10,7 +15,6 @@ $request1 = "CREATE TABLE `users` (
 	`role` VARCHAR(50) DEFAULT NULL,
 	PRIMARY KEY (`id`)
 )";
-
 $request2 = "CREATE TABLE `products` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`article` VARCHAR(100) DEFAULT NULL,
@@ -21,11 +25,14 @@ $request2 = "CREATE TABLE `products` (
 	`auth_id` INT DEFAULT NULL,
 	PRIMARY KEY (`id`)
 )";
-
 $rezult1 = mysqli_query($connection, $request1);
 $rezult2 = mysqli_query($connection, $request2);
+
 echo "<pre>";
-echo "===== USERS TABLE ============================= <br><br>";
+echo "===== DATA BAZE ===============================<br>";
+var_dump($rezult0);
+echo "<br><br>===== USERS TABLE =============================<br>";
 var_dump($rezult1);
+echo "<br><br>===== PRODUCTS TABLE ==========================<br>";
 var_dump($rezult2);
-echo "<br>===== PRODUCTS TABLE ==========================";
+
