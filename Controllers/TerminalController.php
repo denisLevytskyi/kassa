77ff8a0c868d5a39cf339b1a7bc0a94ac4bf7c70
denisLevytskyi <116975058+denisLevytskyi@users.noreply.github.com>
2023-06-25 09:46:59 +0300

    Update TerminalController.php

--- Controllers/TerminalController.php
+++ Controllers/TerminalController.php
@@ -36,28 +36,27 @@ class TerminalController {
 		}
 	}
 
-	protected function get_data ($type) {
-		if ($type == 'checks') {
-			$model = new Models\CheckModel();
-			$checks = $model->get_checks('base_factor', 0);
-			echo serialize($checks);
-		} elseif ($type == 'branches') {
-			$model = new Models\StaffModel();
-			$branches = $model->get_branches('base_factor', 0);
-			echo serialize($branches);
-		} elseif ($type == 'balances') {
-			$model = new Models\StaffModel();
-			$balances = $model->get_balances('base_factor', 0, 0);
-			echo serialize($balances);
-		}
+	protected function get_data () {
+		$model1 = new Models\CheckModel();
+		$model2 = new Models\StaffModel();
+		$model3 = new Models\StaffModel();
+		$checks = $model->get_checks('base_factor', 0);
+		$branches = $model->get_branches('base_factor', 0);
+		$balances = $model->get_balances('base_factor', 0, 0);
+		$data = array (
+			'checks' => $checks,
+			'branches' => $branches,
+			'balances' => $balances
+		);
+		echo serialize($data);
 	}
 
 	public function get_terminal () {
 		if (empty($_POST['terminal_key']) and $_POST['terminal_key'] != 1) {
 			exit();
 		}
-		if (isset($_POST['terminal_code']) and isset($_POST['terminal_data']) and $_POST['terminal_code'] == 1) {
-			$this->get_data($_POST['terminal_data']);
+		if (isset($_POST['terminal_code']) and $_POST['terminal_code'] == 1) {
+			$this->get_data();
 		} elseif (isset($_POST['terminal_code']) and isset($_POST['terminal_data']) and $_POST['terminal_code'] == 2) {
 			$this->get_update($_POST['terminal_data']['type'], $_POST['terminal_data']['id']);
 		} elseif (isset($_POST['terminal_code']) and isset($_POST['terminal_data']) and $_POST['terminal_code'] == 3) {
