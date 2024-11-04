d0db889a0218b66561f3b5fcff3fd09686632612
DENIS STORM <MAIL@MAIL>
2024-11-04 16:14:57 +0200

    DEBUG

--- Scripts/autoload.js
+++ Scripts/autoload.js
@@ -1,12 +1,12 @@
 const get_reload = () => {
-    location.reload();
+	location.reload();
 }
 
 const reload_function =  (time = 2500) => {
-    console.log(time/1000);
-    setTimeout(() => {
-        get_reload();
-    }, time);
+	console.log(time/1000);
+	setTimeout(() => {
+		get_reload();
+	}, time);
 }
 
 reload_function();
--- Scripts/list.js
+++ Scripts/list.js
@@ -1,56 +1,56 @@
 const list_function = (name_index = '', step_index = 10) => {
-    const list = document.getElementById('listWrap' + name_index).children;
-    const previous = document.getElementById('previous' + name_index);
-    const next = document.getElementById('next' + name_index);
-    const all = document.getElementById('all' + name_index);
-    const step = step_index;
+	const list = document.getElementById('listWrap' + name_index).children;
+	const previous = document.getElementById('previous' + name_index);
+	const next = document.getElementById('next' + name_index);
+	const all = document.getElementById('all' + name_index);
+	const step = step_index;
 
-    let index = step;
+	let index = step;
 
-    const add_deactivate = () => {
-        let list_item;
-        for (list_item of list) {
-            list_item.classList.add('deactivate');
-        }
-    }
+	const add_deactivate = () => {
+		let list_item;
+		for (list_item of list) {
+			list_item.classList.add('deactivate');
+		}
+	}
 
-    const remove_deactivate = (start, stop) => {
-        while (start < stop) {
-            list[start].classList.remove('deactivate');
-            start++;
-        }
-    }
+	const remove_deactivate = (start, stop) => {
+		while (start < stop) {
+			list[start].classList.remove('deactivate');
+			start++;
+		}
+	}
 
-    if (list.length > step) {
-        add_deactivate();
-        remove_deactivate(0, step);
-        next.classList.remove('deactivate');
-        previous.classList.remove('deactivate');
-        all.classList.remove('deactivate');
-    }
+	if (list.length > step) {
+		add_deactivate();
+		remove_deactivate(0, step);
+		next.classList.remove('deactivate');
+		previous.classList.remove('deactivate');
+		all.classList.remove('deactivate');
+	}
 
-    next.addEventListener('click', () => {
-        if (index < list.length) {
-            index += step;
-            add_deactivate();
-            remove_deactivate(index - step, index);
-        }
-    })
+	next.addEventListener('click', () => {
+		if (index < list.length) {
+			index += step;
+			add_deactivate();
+			remove_deactivate(index - step, index);
+		}
+	})
 
-    previous.addEventListener('click', () => {
-        if (index > step) {
-            index -= step;
-            add_deactivate();
-            remove_deactivate(index - step, index);
-        }
-    })
+	previous.addEventListener('click', () => {
+		if (index > step) {
+			index -= step;
+			add_deactivate();
+			remove_deactivate(index - step, index);
+		}
+	})
 
-    all.addEventListener('click', () => {
-        let list_item;
-        for (list_item of list) {
-            list_item.classList.remove('deactivate');
-        }
-    })
+	all.addEventListener('click', () => {
+		let list_item;
+		for (list_item of list) {
+			list_item.classList.remove('deactivate');
+		}
+	})
 }
 
 list_function();
